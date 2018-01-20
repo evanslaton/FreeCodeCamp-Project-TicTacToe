ab4cc08a6a2b00a38c3de033166c09fa19ac9522
Evan Slaton <evanslaton@gmail.com>
2018-01-20 04:17:22 -0500

    Added stopGame() function

--- js/js.js
+++ js/js.js
@@ -3,6 +3,10 @@ document.addEventListener("DOMContentLoaded", function() {
 var playerOneTurn = true;
 var oImg = 'img/o.png';
 var xImg = 'img/x.png';
+var win = false;
+var draw = false;
+var fullBoard = 0;
+
 
 var winningCombos = [
   [0, 1, 2],
@@ -15,14 +19,25 @@ var winningCombos = [
   [2, 5, 8]
 ];
 
+
 //Assigns click event to boxes and displays 'O'
-var boxes = document.querySelectorAll('.box');
+var boxes = document.getElementsByClassName('box');
 
 for (i = 0; i < boxes.length; i++) {
-  boxes[i].addEventListener('click', function() {
-    var clicked = document.getElementById(this.id);
-    var addImageTo = clicked.firstElementChild;
-    addImageTo.style.display = 'block';
+  boxes[i].addEventListener('click', playGame);
+}
+
+
+//Starts the game
+function playGame() {
+  var clicked = document.getElementById((this.id));
+  var addImageTo = clicked.firstElementChild;
+
+  //Allows each box to be selected only once
+  if (!clicked.classList.contains('clicked')) {
+    clicked.classList.add('clicked');
+    addImageTo.style.display = 'block';  
+    fullBoard++;
 
     if (playerOneTurn) {
       clicked.classList.add('o');
@@ -35,45 +50,53 @@ for (i = 0; i < boxes.length; i++) {
       checkWin('x');
       playerOneTurn = true;
     }
-  })
+  }
 }
 
 
-
-
 //Checks if a player has won
-var checkWin = (n) => {
+var checkWin = (ox) => {
   var checkForWin = [];
+  var counter;
 
   //Pushes all selected elements into checkForWin array
   for (i = 0; i < boxes.length; i++) {
-    if (boxes[i].classList.contains(n)) {
+    if (boxes[i].classList.contains(ox)) {
       if (checkForWin.indexOf(i) === -1) {
         checkForWin.push(i);
       }
     }
   }
 
-  //Check for a win
+  //Checks for a win
   for (i = 0; i < winningCombos.length; i++) {
-    var counter = 0;
+    counter = 0;
     for (j = 0; j < 3; j++) {
       if (checkForWin.indexOf(winningCombos[i][j]) !== -1) {
         counter++;
         if (counter === 3) {
-          console.log('Player ' + n + ' wins!')
+          win = true;
+          console.log('Player ' + ox + ' wins!')
+          stopGame();
         }
       }
     }
   }
 
-  // console.log(checkForWin);
-  // console.log(checkForWin.indexOf(winningCombos[0][0]));
+  //Determines if the game is a draw
+  if (win === false && fullBoard === 9) {
+    draw = true;
+    console.log('It\'s a draw');
+    stopGame();    
+  }
+};
 
 
-
-
-
+//Ends the game
+var stopGame = () => {
+  for (i = 0; i < boxes.length; i++) {
+    boxes[i].removeEventListener('click', playGame);
+  }
 };
 
 
