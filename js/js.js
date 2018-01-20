74c5bc4b4c8f0e9af4e4e2af00e8a02e418992c1
Evan Slaton <evanslaton@gmail.com>
2018-01-20 00:39:39 -0500

    Added click event to boxes that displays 'O' when clicked

--- js/js.js
+++ js/js.js
@@ -1,4 +1,15 @@
 $('document').ready(function() {
 
+  //Assigns click event to boxes and displays 'O'
+  var boxes = document.querySelectorAll('.box');
+
+  for (i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener('click', function () {
+      var clicked = document.getElementById(this.id).firstElementChild;
+      clicked.src = 'img/o.png';
+      clicked.style.display = 'block';
+    })
+  }
+
 
 });
