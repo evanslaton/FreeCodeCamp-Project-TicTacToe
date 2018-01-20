51dbc6c65b3a28850ca7a486ff65441b2605ef8f
Evan Slaton <evanslaton@gmail.com>
2018-01-20 08:12:18 -0500

    Added computer player using Math by selecting random available spaces

--- js/js.js
+++ js/js.js
@@ -1,12 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 var playerOneTurn = true;
-var oImg = 'img/o.png';
-var xImg = 'img/x.png';
+var playerOne = 'o';
+var playerTwo = 'x';
+var playerOneImg = 'img/' + playerOne + '.png';
+var playerTwoImg = 'img/' + playerTwo + '.png';
 var win = false;
 var draw = false;
 var fullBoard = 0;
+var checkForWin = [];
 
+//Boxes
+var zero = document.getElementById('0');
+var one = document.getElementById('1');
+var two = document.getElementById('2');
+var three = document.getElementById('3');
+var four = document.getElementById('4');
+var five = document.getElementById('5');
+var six = document.getElementById('6');
+var seven = document.getElementById('7');
+var eight = document.getElementById('8');
 
 var winningCombos = [
   [0, 1, 2],
@@ -35,29 +48,89 @@ function playGame() {
 
   //Allows each box to be selected only once
   if (!clicked.classList.contains('clicked')) {
-    clicked.classList.add('clicked');
-    addImageTo.style.display = 'block';  
-    fullBoard++;
 
     if (playerOneTurn) {
-      clicked.classList.add('o');
-      addImageTo.src = oImg;
-      checkWin('o');
+      clicked.classList.add(playerOne);
+      addImageTo.src = playerOneImg;
+      clicked.classList.add('clicked');
+      addImageTo.style.display = 'block';  
+      fullBoard++;      
+      checkWin(playerOne);
       playerOneTurn = false;
+      computerPlay(playerOne);
     } else {
-      clicked.classList.add('x');
-      addImageTo.src = xImg;
-      checkWin('x');
-      playerOneTurn = true;
+
+
+
+
+      // clicked.classList.add('x');
+      // addImageTo.src = xImg;
+      // checkWin('x');
+      // playerOneTurn = true;
     }
   }
 }
 
 
+//Computer's turn
+var computerPlay = (ox) => {
+  setTimeout(function() {
+    var openBoxes = [];
+    var playHere;
+    var randomBox;
+    checkForWin = [];
+
+      for (i = 0; i < boxes.length; i++) {
+        if (!boxes[i].classList.contains('clicked')) {
+          openBoxes.push(boxes[i]);
+        }
+      }
+
+      if (openBoxes.indexOf(four) !== -1 && openBoxes.length === 8) {
+        four.classList.add(playerTwo);
+        four.firstElementChild.src = playerTwoImg;
+        four.firstElementChild.style.display = 'block';
+        four.classList.add('clicked');
+      } else if (openBoxes.indexOf(four) === -1 && openBoxes.length === 8) {
+        zero.classList.add(playerTwo);
+        zero.firstElementChild.src = playerTwoImg;
+        zero.firstElementChild.style.display = 'block';
+        zero.classList.add('clicked'); 
+      } else if (openBoxes.length < 8) {
+
+        //Pushes all selected elements into checkForWin array
+        for (i = 0; i < boxes.length; i++) {
+          if (boxes[i].classList.contains(ox)) {
+            if (checkForWin.indexOf(i) === -1) {
+              checkForWin.push(i);
+            }
+          }
+        }
+
+        randomBox = randomNumber(0, openBoxes.length -1);       
+        playHere = openBoxes[randomBox];
+        playHere.classList.add(playerTwo);
+        playHere.firstElementChild.src = playerTwoImg;
+        playHere.firstElementChild.style.display = 'block';
+        playHere.classList.add('clicked');
+      }
+
+      checkWin(playerTwo);
+      playerOneTurn = true;
+  }, 500);
+};
+
+
+//Generates a random number
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
 //Checks if a player has won
 var checkWin = (ox) => {
-  var checkForWin = [];
-  var counter;
+  checkForWin = [];
+  counter = 0;
 
   //Pushes all selected elements into checkForWin array
   for (i = 0; i < boxes.length; i++) {
